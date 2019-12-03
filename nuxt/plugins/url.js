4713584bbc175da8f8583afd6227d328ab57139b
brian lee <cn_brian@163.com>
2019-12-03 19:00:40 +0800

    修复nuxt打包存在babel版本问题

--- nuxt/plugins/url.js
+++ nuxt/plugins/url.js
@@ -1,2 +1,2 @@
 /*判断环境使用不同接口*/
-export let baseurl = process.env.NODE_ENV === 'production' ? 'http://api.brianlee.cn' : 'http://localhost:3000';
+export let baseurl = process.env.NODE_ENV === 'production' ? 'https://api.brianlee.cn' : 'http://localhost:3000';
