54aef9a3fe8e7ec7f420d1e07e7d99b68d1ba47e
brian lee <cn_brian@163.com>
2021-05-14 15:17:47 +0800

    增加网站底部信息

--- server/migration/1620635482952-insert_options_table_site.ts
+++ server/migration/1620635482952-insert_options_table_site.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class insertOptionsTableSite1620635482952 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query("INSERT INTO `options` (`key`, `value`) VALUES ('site_config', '{\"title\": \"\", \"description\": \"\", \"keywords\": \"\", \"author\": \"\"}')");
+      await queryRunner.query("INSERT INTO `options` (`key`, `value`) VALUES ('site_config', '{\"title\": \"\", \"description\": \"\", \"keywords\": \"\", \"author\": \"\", \"record\": \"\"}')");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
