ce005e7498e407e1e6d02acc7b05cf32ac71866e
brian lee <cn_brian@163.com>
2021-05-22 12:58:28 +0800

    server 增加docker部署

--- server/modules/migration/1615288207549-add_comment_table.ts
+++ server/modules/migration/1615288207549-add_comment_table.ts
@@ -16,6 +16,7 @@ export class addCommentTable1615288207549 implements MigrationInterface {
           \`sensitive\` TEXT DEFAULT NULL COMMENT "敏感词组",
           pass INT(1) DEFAULT 1 COMMENT "是否通过敏感词校验",
           original TEXT NOT NULL COMMENT "原始评论内容",
+          parentId INT(11) DEFUALT 0 COMMENT "父级id",
           createdAt INT(11) DEFAULT 0 COMMENT "创建时间",
           updatedAt INT(11) DEFAULT 0 COMMENT "最后更新时间"
         )
